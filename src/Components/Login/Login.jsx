0fda3be538f20e5f0412110d6eba22bebd27fc54
sjiya114 <sjiya04072004@gmail.com>
2025-05-24 11:19:12 +0530

    editing files

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -31,7 +31,7 @@ function Login() {
               <input onChange={(e)=>{setEmail(e.target.value)}} className='border-2 px-2 py-1 mb-2 w-full border-gray-400 rounded-md mt-4'  placeholder='Enter email address' name='Email' type="text" required /></div>
             <div>
                <label htmlFor="Password"  className='block ml-3 absolute bg-gray-200'>Password</label>
-              <input onChange={(e)=>{setPassword(e.target.value)}} className='border-2 px-2 py-1 mb-1 w-full border-gray-400 rounded-md mt-4' placeholder='Enter password'  name='Password' type="number" required /></div>
+              <input onChange={(e)=>{setPassword(e.target.value)}} className='border-2 px-2 py-1 mb-1 w-full border-gray-400 rounded-md mt-4' placeholder='Enter password'  name='Password' type="password" required /></div>
                <div className='mt-4'>
               <button className='bg-gray-400 text-center w-full rounded-md px-4 py-2 text-gray-950 hover:bg-purple-600 hover:text-white'>Login</button>
            </div>
